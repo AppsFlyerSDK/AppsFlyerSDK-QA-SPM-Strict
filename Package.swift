2fdb46bfe48f10fc1de8c70fdd4044e67edbfc57
Amit.Levy <amit.levy@appsflyer.com>
2025-03-09 14:22:13 +0200

    Package.swift file has been updated to 6.16.2.228

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.16.1.227/AppsFlyerLib-Strict-SPM.xcframework.zip",
-            checksum: "66627dc7f0f492982a6428d01fbc932c709cb8d24f50d92da223cfe4034aa2c0"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.16.2.228/AppsFlyerLib-Strict-SPM.xcframework.zip",
+            checksum: "298d9fc83e7b892d47538e985c5e4c5dd42437d43f4b3358842aeef5b31fc90e"
         )
     ]
 )
