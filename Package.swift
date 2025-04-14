c97df773082ae22ae39eea475f4c5927f3fe6891
morisgateno-appsflyer <moris.gateno@appsflyer.com>
2025-04-14 16:20:54 +0300

    Package.swift file have been updated to 6.17.0.19450585

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:5.3
 import PackageDescription
-
 let package = Package(
     name: "AppsFlyerLib",
     products: [
@@ -11,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.16.2.229/AppsFlyerLib-Strict-SPM.xcframework.zip",
-            checksum: "d2b9d54921b795bebbf90788bf8501da4e63e88f00935c27b78eea00b8aa012c"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.17.0.19450585/AppsFlyerLib-Strict-SPM.xcframework.zip",
+            checksum: "eb2f088f866f230c66990f9ec405396744f0cbff5278f8f1337b603ab173e8da"
         )
     ]
 )
