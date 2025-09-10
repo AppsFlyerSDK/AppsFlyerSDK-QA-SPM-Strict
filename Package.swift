1cf6181869ed77da8fd9ae12fc5293191360768c
ec2-user <ec2-user@druidindex-20070-030-prod.eu1.appsflyer.com>
2025-09-10 14:45:27 +0000

    Package.swift file have been updated to 6.17.6.25304594

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.17.5.24949228/AppsFlyerLib-Strict-SPM.xcframework.zip",
-            checksum: "aa1502040ed05cb911abaa7494c1910c60ed26b6dbaf007312f3c0858ead74df"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-sdk-qa/releases/download/6.17.6.25304594/AppsFlyerLib-Strict-SPM.xcframework.zip",
+            checksum: "9ed3808288d6bdde05986c38021ea5a7bd0c12f82d5b72346c05b018fe05f992"
         )
     ]
 )
